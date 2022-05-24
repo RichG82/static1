eedecf55ed1de2f3ff16dcd2132ec120c7326bf7
Rich Grenwick <rgrenwick@gmail.com>
2022-05-24 15:09:23 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+function doWork() {
+  fetch("api/StaticTrigger1?code=7xo8ExWeo6QZ2n1Pg9ApzzoB-P1clp8ExroFSDOpC70zAzFuktM1YQ==").then(async(result)=> {
+    document.getElementById("content").innerHTML = await result.text()
+  });
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,8 +15,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
 
-          Hello world!
+          https://ashy-forest-02c92b810.1.azurestaticapps.net/api/StaticTrigger1?code=7xo8ExWeo6QZ2n1Pg9ApzzoB-P1clp8ExroFSDOpC70zAzFuktM1YQ==
         </p>
+        <button onClick={()=> doWork()}>Click Me</button>
+        <div id="content">
+          
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
